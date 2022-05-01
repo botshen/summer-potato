76efaba7da6ee8ce8e407ee44345d7e0b5771d98
shenxin <wb_shenxin@meituan.com>
2022-05-02 00:07:15 +0800

    feat: 搭建框架配置 scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./views/Login";
+// App.js
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/login">login</Link>
+      </nav>
+    </>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/views/Login.tsx
@@ -0,0 +1,11 @@
+import s from "./Login.module.scss";
+
+function Login() {
+  return (
+    <div className={s.wrapper}>
+      <h1>夏天闹钟登录 ⏰</h1>
+    </div>
+  );
+}
+
+export default Login;
