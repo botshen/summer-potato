ee8c62bd1dac0c067add52c92c92535c41f26c54
shenxin <wb_shenxin@meituan.com>
2022-05-02 00:30:23 +0800

    feat: mantine UI 框架和登录页面

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,9 +1,19 @@
 import s from "./Login.module.scss";
+import { Button, PasswordInput, Input } from "@mantine/core";
+import { At } from "tabler-icons-react";
 
 function Login() {
   return (
     <div className={s.wrapper}>
-      <h1>夏天闹钟登录 ⏰</h1>
+      <h2>夏天闹钟登录 ⏰</h2>
+      <Input className={s.input} icon={<At />} placeholder="请输入你的用户名" />
+      <PasswordInput
+        className={s.input}
+        placeholder="请输入你的密码"
+        required
+      />
+      <Button className={s.login}>登录</Button>
+      <span>如果你没有有账号，请立即注册</span>
     </div>
   );
 }
